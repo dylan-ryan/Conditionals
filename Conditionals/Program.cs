f34992f2f038d6b8c912621ba75472284822c9fc
Dylan <dylanryan431@gmail.com>
2023-09-25 08:46:58 -0300

    Putitongit

---
+++ Conditionals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditionals:");
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
