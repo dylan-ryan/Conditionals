862c8cd675b1203806725e184cdd94a8c930b114
Dylan <dylanryan431@gmail.com>
2023-09-26 15:48:24 -0300

    Pain
    spent an insane amount of time on the gun hud

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -11,6 +11,8 @@ namespace Conditionals
         //declarations
         static int health;
         static int lives;
+        static string currentweapon;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Conditionals:");
@@ -22,11 +24,14 @@ namespace Conditionals
             lives = 3;
 
             //sample gameplay
+            ChooseGun(0);
             ShowHud();
+            ChooseGun(1);
             TakeDamage(10);
             ShowHud();
             TakeDamage(50);
             ShowHud();
+            ChooseGun(5);
             TakeDamage(50);
             ShowHud();
 
@@ -36,6 +41,40 @@ namespace Conditionals
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
+
+        static void ChooseGun(int weapon)
+        {
+            if (weapon == 0)
+            {
+                currentweapon = "pistol";
+            }
+
+            if (weapon == 1)
+            {
+                currentweapon = "shotgun";
+            }
+
+            if (weapon == 2) 
+            {
+                currentweapon = "spreader";
+            }
+
+            if (weapon == 3)
+            {
+                currentweapon = "laser";
+            }
+
+            if (weapon == 4)
+            {
+                currentweapon = "sniper";
+            }
+
+            if (weapon == 5)
+            {
+                currentweapon = "BFG";
+            }
+        }
+
 
         static void TakeDamage(int damage)
         {
@@ -58,12 +97,14 @@ namespace Conditionals
                 //set health to be 0
                 health = 0;
             }        
+
         }
 
         static void ShowHud()
         {
             Console.WriteLine();
 
+            Console.WriteLine(currentweapon);
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Lives: " + lives);
 
